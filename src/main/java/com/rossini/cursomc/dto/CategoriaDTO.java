f9bfa32077cb8e3d8007fa8b1ff1804393e988ac
Rossini <RossiniADS@users.noreply.github.com>
2020-08-25 02:58:06 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/rossini/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/rossini/cursomc/dto/CategoriaDTO.java
@@ -1,11 +1,8 @@
 package com.rossini.cursomc.dto;
 
 import java.io.Serializable;
-
 import javax.validation.constraints.NotEmpty;
-
 import org.hibernate.validator.constraints.Length;
-
 import com.rossini.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -13,8 +10,8 @@ public class CategoriaDTO implements Serializable {
 
 	private Integer id;
 
-	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
