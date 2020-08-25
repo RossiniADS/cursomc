249b23cda1899a0bcca2f497c6fc6294dd5609fb
Rossini <RossiniADS@users.noreply.github.com>
2020-08-25 19:34:11 -0300

    validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/rossini/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/rossini/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.rossini.cursomc.domain.Cliente;
 import com.rossini.cursomc.domain.enums.TipoCliente;
 import com.rossini.cursomc.dto.ClienteNewDTO;
+import com.rossini.cursomc.repositories.ClienteRepository;
 import com.rossini.cursomc.resources.exception.FieldMessage;
 import com.rossini.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -25,8 +32,14 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
+		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
