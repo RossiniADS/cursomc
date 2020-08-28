fadce436430d624ddae543cf4666aad22c4a5f93
Rossini <RossiniADS@users.noreply.github.com>
2020-08-28 18:18:36 -0300

    Email HTML

--- src/main/java/com/rossini/cursomc/services/EmailService.java
+++ src/main/java/com/rossini/cursomc/services/EmailService.java
@@ -1,12 +1,18 @@
 package com.rossini.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.rossini.cursomc.domain.Pedido;
 
 public interface EmailService {
-	
+
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
