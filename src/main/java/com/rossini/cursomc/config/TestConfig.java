7fd9e54a51378f761d3dd8a2764c5121f32fe344
Rossini <RossiniADS@users.noreply.github.com>
2020-08-28 17:12:00 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/rossini/cursomc/config/TestConfig.java
+++ src/main/java/com/rossini/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.rossini.cursomc.services.DBService;
+import com.rossini.cursomc.services.EmailService;
+import com.rossini.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -19,5 +21,10 @@ public class TestConfig {
 	public boolean instantiateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 }
