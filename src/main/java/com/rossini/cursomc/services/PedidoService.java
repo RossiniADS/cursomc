246bd6cb74bbcd60c6a43a7a5d7e6b7184cbff78
Rossini <RossiniADS@users.noreply.github.com>
2020-08-29 20:23:31 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/rossini/cursomc/services/PedidoService.java
+++ src/main/java/com/rossini/cursomc/services/PedidoService.java
@@ -4,9 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.rossini.cursomc.domain.Cliente;
 import com.rossini.cursomc.domain.ItemPedido;
 import com.rossini.cursomc.domain.PagamentoComBoleto;
 import com.rossini.cursomc.domain.Pedido;
@@ -14,6 +17,8 @@ import com.rossini.cursomc.domain.enums.EstadoPagamento;
 import com.rossini.cursomc.repositories.ItemPedidoRepository;
 import com.rossini.cursomc.repositories.PagamentoRepository;
 import com.rossini.cursomc.repositories.PedidoRepository;
+import com.rossini.cursomc.security.UserSS;
+import com.rossini.cursomc.services.exceptions.AuthorizationException;
 import com.rossini.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -46,7 +51,6 @@ public class PedidoService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 
-	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
@@ -66,8 +70,17 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		emailService.sendOrderConfirmationHtmlEmail(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 }
